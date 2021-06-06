ebe78889507f25e389853d14bd4ab7a6fafe41a6
Hami Lemon <hamilemon@aliyun.com>
2021-06-06 22:31:36 +0800

    修改communicate.js

--- js/communicate.js
+++ js/communicate.js
@@ -1,87 +1,24 @@
 /* 与后端通信 */
 let host = "http://localhost:8080/Wabby_Wabbo/";
-let tips_data = [{
-        id: 1,
-        date: "2020-02-02 12:12:22",
-        type: "知识",
-        starNum: 7,
-        content: "demofasdf"
-    },
-    {
-        id: 2,
-        date: "2020-02-02 12:12:22",
-        type: "知识",
-        starNum: 7,
-        content: "demofasdf"
-    }, {
-        id: 3,
-        date: "2020-02-02 12:12:22",
-        type: "知识",
-        starNum: 7,
-        content: "demofasdf"
-    }, {
-        id: 4,
-        date: "2020-02-02 12:12:22",
-        type: "知识",
-        starNum: 7,
-        content: "demofasdf"
-    }, {
-        id: 5,
-        date: "2020-02-02 12:12:22",
-        type: "知识",
-        starNum: 7,
-        content: "demofasdf"
-    }, {
-        id: 6,
-        date: "2020-02-02 12:12:22",
-        type: "知识",
-        starNum: 7,
-        content: "demofasdf"
-    }
-];
-let comments_data = [{
+let tips_data = {
+    id: 1,
+    date: "2020-02-02 12:12:22",
+    type: "知识",
+    starNum: 7,
+    content: "demofasdf"
+};
+let comments_data = {
     id: 1,
     content: "你好",
     starNum: 10,
     date: "2020-02-02 23:12:39",
     tips_id: 1
-}, {
-    id: 1,
-    content: "你好",
-    starNum: 10,
-    date: "2020-02-02 23:12:39",
-    tips_id: 1
-}, {
-    id: 1,
-    content: "你好",
-    starNum: 10,
-    date: "2020-02-02 23:12:39",
-    tips_id: 1
-}, {
-    id: 1,
-    content: "你好",
-    starNum: 10,
-    date: "2020-02-02 23:12:39",
-    tips_id: 1
-}, {
-    id: 1,
-    content: "你好",
-    starNum: 10,
-    date: "2020-02-02 23:12:39",
-    tips_id: 1
-}, {
-    id: 1,
-    content: "你好",
-    starNum: 10,
-    date: "2020-02-02 23:12:39",
-    tips_id: 1
-}]
+};
 let response = {
     code: 200,
     msg: "ok",
-    data: {}
+    data: []
 }
-
 /* 发布帖子 */
 function postTips(data) {
     let url = host + "/posttips";
@@ -95,15 +32,19 @@ function postTips(data) {
 function getTips(type, page) {
     let url = host + "/gettips?type=&page=";
     let r = response;
-    r.data = tips_data;
+    r.data = [];
+    for (let i = 0; i < 12; i++) {
+        r.data.push(tips_data)
+        r.data[i].id = i + 1;
+    }
     return r;
-
 }
 /* 根据id获取帖子 */
 function getTipsById(id) {
     let url = host + "/getdetail?id=";
     let r = response;
-    r.data = tips_data[1];
+    r.data = tips_data;
+    r.data.id = id;
     return r;
 }
 /* 发布评论 */
@@ -120,18 +61,33 @@ function postComments(comments) {
 function getComments(tipsId, page) {
     let url = host + "/getcomments?tid=&page=";
     let r = response;
-    r.data = comments_data;
+    r.data = [];
+    for(let i = 0; i < 10; i++){
+        r.data.push(comments_data);
+        r.data[i].id = i + 1;
+    }
     return r;
 
 }
 /* 获取热贴 */
 function getHotTips() {
     let url = host + "/gethottips";
-    return tips_data;
-
+    let r = response;
+    r.data = [];
+    for(let i = 0; i < 10; i++){
+        r.data.push(comments_data);
+        r.data[i].id = i + 1;
+    }
+    return r;
 }
 /* 获取热评 */
 function getHotComments(tipsId) {
     let url = host + "/gethotcom?tid=";
-    return comments_data;
+    let r = response;
+    r.data = [];
+    for(let i = 0; i < 3; i++){
+        r.data.push(comments_data);
+        r.data[i].id = i + 1;
+    }
+    return r;
 }

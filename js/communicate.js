4ef763eed8b198d30f113eb865a182f498858603
Hami Lemon <hamilemon@aliyun.com>
2021-06-07 12:54:47 +0800

    修改communicate.js

--- js/communicate.js
+++ js/communicate.js
@@ -14,11 +14,7 @@ let comments_data = {
     date: "2020-02-02 23:12:39",
     tips_id: 1
 };
-let response = {
-    code: 200,
-    msg: "ok",
-    data: []
-}
+
 /* 发布帖子 */
 function postTips(data) {
     let url = host + "/posttips";
@@ -31,28 +27,43 @@ function postTips(data) {
 /* 获取帖子 */
 function getTips(type, page) {
     let url = host + "/gettips?type=&page=";
-    let r = response;
-    r.data = [];
-    for (let i = 0; i < 12; i++) {
-        r.data.push(tips_data)
-        r.data[i].id = i + 1;
+    let r = {
+        code: 200,
+        msg: "ok",
+        data: []
+    }
+    for (let i = 0; i < 10; i++) {
+        r.data.push({
+            id: i + 1,
+            content: "测试",
+            date: "2020-02-02 12:12:22",
+            type: "知识",
+            starNum: 7
+        })
     }
     return r;
 }
 /* 根据id获取帖子 */
 function getTipsById(id) {
     let url = host + "/getdetail?id=";
-    let r = response;
-    r.data = tips_data;
-    r.data.id = id;
-    return r;
+
+    return {
+        code: 200,
+        msg: "",
+        data: {
+            id: id,
+            starNum: 2,
+            type: "demo",
+            content: "dsfsafa"
+        }
+    };
 }
 /* 发布评论 */
 function postComments(comments) {
     let url = host + "/postcomment";
     let r = {
         code: 200,
-        msg: "ok"
+        msg: "ok",
     }
     return r;
 
@@ -60,11 +71,19 @@ function postComments(comments) {
 /* 获取评论 */
 function getComments(tipsId, page) {
     let url = host + "/getcomments?tid=&page=";
-    let r = response;
-    r.data = [];
-    for(let i = 0; i < 10; i++){
-        r.data.push(comments_data);
-        r.data[i].id = i + 1;
+    let r = {
+        code: 200,
+        msg: "ok",
+        data: []
+    }
+    for (let i = 0; i < 10; i++) {
+        r.data.push({
+            id: i + 1,
+            content: "你好",
+            starNum: 10,
+            date: "2020-02-02 23:12:39",
+            tips_id: 1
+        })
     }
     return r;
 
@@ -72,22 +91,38 @@ function getComments(tipsId, page) {
 /* 获取热贴 */
 function getHotTips() {
     let url = host + "/gethottips";
-    let r = response;
-    r.data = [];
-    for(let i = 0; i < 10; i++){
-        r.data.push(comments_data);
-        r.data[i].id = i + 1;
+    let r = {
+        code: 200,
+        msg: "ok",
+        data: []
+    }
+    for (let i = 0; i < 10; i++) {
+        r.data.push({
+            id: i + 1,
+            content: "测试",
+            date: "2020-02-02 12:12:22",
+            type: "知识",
+            starNum: 7
+        })
     }
     return r;
 }
 /* 获取热评 */
 function getHotComments(tipsId) {
     let url = host + "/gethotcom?tid=";
-    let r = response;
-    r.data = [];
-    for(let i = 0; i < 3; i++){
-        r.data.push(comments_data);
-        r.data[i].id = i + 1;
+    let r = {
+        code: 200,
+        msg: "ok",
+        data: []
+    }
+    for (let i = 0; i < 3; i++) {
+        r.data.push({
+            id: i + 1,
+            content: "你好",
+            starNum: 10,
+            date: "2020-02-02 23:12:39",
+            tips_id: 1
+        })
     }
     return r;
 }
